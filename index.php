bb7283be7edebc7f774e861c59336fc9fbc1ba02
Kingster <kingsterrck@126.com>
2022-05-25 15:56:39 +0800

    Add jQuery and font, update demo file

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=divice-width, initial-scale=1">
+    <title>BOOTSTRAP</title>
+    <style>
+        div {
+            height: 200px;
+        }
+        .container-md {
+            background-color: lightcoral;
+        }
+        .container-lg {
+            background-color: gold;
+        }
+        * {
+            font-family: 'Kanit';
+        }
+    </style>
+    <link type="text/css" rel="stylesheet" href="css/bootstrap-grid.css">
+</head>
+<body>
+    <h1>OMEGALUL IT WORKS</h1>
+    <div class="container-md">
+
+    </div>
+    <div class="container-lg">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+        </div>
+    </div>
+<script src="js/jquery-1.12.4.js"></script>
+<script src="js/bootstrap.js"></script>
+</body>
