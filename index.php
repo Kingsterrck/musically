dfee6b2c93c38435ba62b1bfabae36e2089d5d30
Kingster <kingsterrck@126.com>
2022-05-26 15:53:03 +0800

    Make font-face actually work

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
     <meta name="viewport" content="width=divice-width, initial-scale=1">
     <title>BOOTSTRAP</title>
     <style>
+        @font-face {
+            font-family: 'Kanit';
+            src: url("css/fonts/Kanit-Regular.ttf");
+        }
         div {
             height: 200px;
         }
@@ -17,10 +21,12 @@
             background-color: gold;
         }
         * {
-            font-family: 'Kanit';
+            font-family: 'Kanit',sans-serif;
         }
+
     </style>
     <link type="text/css" rel="stylesheet" href="css/bootstrap-grid.css">
+
 </head>
 <body>
     <h1>OMEGALUL IT WORKS</h1>
